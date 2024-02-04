6b81896feacd6ac0e858842bf03fd7b547024db1
Johann Hahn <wanische@gmail.com>
2024-02-04 16:37:56 +0100

    first example : game of life

--- cell_automata.h
+++ cell_automata.h
@@ -1,15 +1,54 @@
 #pragma once
-#include <cinttypes>
+#include <iostream>
+
+enum Automata_Type {
+    ONE_DIM, TWO_DIM
+};
 
 template<typename T> class Cell_Automat {
 public:
-    Cell_Automat() {};
-    Cell_Automat(T* data, size_t size, void (*rules) (const T* data, T* output, size_t size)):
-	data(data), size(size), rules(rules){};
+    Cell_Automat(size_t size, void (*rules) (const T* data, T* output, size_t size), T zero_value, T* new_input = NULL):
+	zero(zero_value), size(size), rules(rules) {
+	cells = new T[size];
+	empty = new T[size];
+	if(new_input) {
+	    memcpy(cells, new_input, sizeof(T) * size);
+	}
+	else {
+	    set_buf(cells, size, zero);
+	}
+	set_buf(empty, size, zero);
+    };
+    ~Cell_Automat() {
+	delete[] cells;
+	delete[] empty;
+    };
+
     size_t size;
-    T* data;
-    void (*rules) (const T* input, T* output, size_t size);
+    T* empty;
+    T* cells;
+
+    void set_input(T* new_input) {
+	memcpy(cells, new_input, sizeof(T) * size);
+    }
     void apply_rules() {
-	rules(data, data, size);
+	rules(cells, empty, size);
+	switch_buffers();
+    }
+    static void set_buf(T* buf, size_t size, T val) {
+	for(int i = 0; i < size; ++i) {
+	    buf[i] = val;
+	}
+    }
+
+private:
+
+    T zero;
+
+    void (*rules) (const T* input, T* output, size_t size);
+    void switch_buffers() {
+	T* h = cells;
+	cells = empty;
+	empty = h;
     }
 };
--- main.cpp
+++ main.cpp
@@ -1,36 +1,101 @@
 #include "raylib/src/raylib.h"
 #include "cell_automata.h"
 #include <iostream>
-#include <cstdint>
+#include <cassert>
 
 typedef uint32_t u32;
 
-Image* img;
+#define COLOR_FROM_U32(c) *(Color*)&c
 
+u32 alive_col = 0xFF111111;
+u32 dead_col  = 0xFFABABAB;
+size_t cells_width = 1000;
+size_t cells_height = 1000;
+float window_width = 1900;
+float window_height = 1200;
+float min_dim = std::min(window_width, window_height);
+Rectangle view_area = {0, 0, min_dim, min_dim};
+
+
+int count_neighbours(int x, int y, const u32* pixels) {
+    int count = 0;
+    for (int y_n = -1; y_n <= 1; ++y_n) {
+	for (int x_n = -1; x_n <= 1; ++x_n) {
+	    int index = x + x_n + (y + y_n) * cells_width;
+	    if(index >= 0 && index < cells_width * cells_height) {
+		if (pixels[index] == alive_col) count++; 
+	    }
+	}
+    }
+    return count;
+}
 void rules(const u32* input, u32* output, size_t size) {
-    for (size_t i = 0; i < size; ++i) {
-	output[i] = GetRandomValue(0x000000FF, 0xFFFFFFFF);	
+    for (int y = 0; y < cells_height; ++y) {
+	for (int x = 0; x < cells_width; ++x) {
+	    int index = x + y * cells_width;
+	    u32 input_val = input[index];
+	    assert(input_val == alive_col || input_val == dead_col && "wrong color");
+
+	    int neighbours = count_neighbours(x, y, input); 
+	    bool alive = (input_val == alive_col);
+
+	    if(alive && neighbours == 3) output[index] = alive_col;
+	    else if(alive && neighbours == 2) output[index] = alive_col;
+	    else if(alive && neighbours < 2) output[index] = dead_col;
+	    else if(alive && neighbours > 3) output[index] = dead_col;
+	    else if(!alive && neighbours == 3) output[index] = alive_col;
+	    else if(!alive && neighbours != 3) output[index] = dead_col;
+	    else {
+		assert(0 && "unreachable");
+	    }
+	}
     }
 }
+void resize() {
+    window_width = GetScreenWidth();
+    window_height = GetScreenHeight();
+    min_dim = std::min(window_width, window_height);
+    view_area = {0, 0, min_dim, min_dim};
+}
+
+
 int main() {
-    std::cout << "hi\n";
+    InitWindow(window_width, window_height, "hi");
+    SetWindowState(FLAG_WINDOW_RESIZABLE);
+   // SetTargetFPS(10);
 
-    InitWindow(800, 600, "hi");
+    Image h = GenImageColor(cells_width, cells_height, COLOR_FROM_U32(alive_col)); 
+    u32* pixels = (u32*)h.data;
+    Color col = *(Color*)&pixels[0];
 
-    Image h = GenImageColor(800, 600, BLACK); 
-    img = &h;
-    Texture txt = LoadTextureFromImage(*img);
+    for (int i = 0; i < cells_width*cells_height; ++i) {
+	if (GetRandomValue(0,1)) {
+	    SetPixelColor(&pixels[i], COLOR_FROM_U32(dead_col), h.format);
+	}
+    }
+    SetPixelColor(&pixels[101], COLOR_FROM_U32(dead_col), h.format);
+    SetPixelColor(&pixels[(int)window_width + 101], COLOR_FROM_U32(dead_col), h.format);
+    Texture txt = LoadTextureFromImage(h);
 
-    Cell_Automat<u32> cell_automat((u32*)img->data, 800*600, rules);
-
+    Cell_Automat<u32> cell_automat(cells_width*cells_height, rules, dead_col, (u32*)h.data);
+    UnloadImage(h);
+    int count = 0;
     while (!WindowShouldClose()) {
-
+	if(IsWindowResized()) {
+	    resize();
+	}
 	BeginDrawing();
-	DrawTexture(txt, 0, 0, WHITE);
+	DrawTexturePro(txt, {0, 0, (float)txt.width, (float)txt.height}, view_area, {0, 0}, 0, WHITE);
+	DrawRectangle(0, 0, 200, 200, col);
+	DrawFPS(0, 0);
 	EndDrawing();
 
-	cell_automat.apply_rules();
-	UpdateTexture(txt, img->data);
+	UpdateTexture(txt, cell_automat.cells);
+	if(count < 10) count++;
+	else cell_automat.apply_rules();
     }
+
+    UnloadTexture(txt);
+    CloseWindow();
     return 0;
 }
