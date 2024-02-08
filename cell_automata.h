4a7322a028849329ad93ff5829d17d9aaf178912
Johann Hahn <johann-hahn@yandex.ru>
2024-02-08 16:15:49 +0100

    input 1d ruleset bits in the ui

--- cell_automata.h
+++ cell_automata.h
@@ -1,15 +1,20 @@
 #pragma once
+#include <cstdint>
 #include <cstdlib>
 #include <ctime>
 #include <iostream>
 #include <cassert>
 
+typedef uint64_t u64;
 
 enum Automata_Type {
     ONE_DIM, TWO_DIM, AUTOMATA_TYPE_MAX
 };
 
 #define INDEX(x, y, width) (x) + ((y) * (width))
+#define BIT_AT(i, map) ((map) >> (i) & u64(1))
+#define BIT_SET(i, map) ((map) |= (u64(1) << (i)))
+#define BIT_RESET(i, map) ((map) &= ~(u64(1) << (i))) 
 
 template<typename T> class Cell_Automat {
 public:
@@ -51,14 +56,39 @@ public:
     T zero;
     T one;
     Automata_Type type;
+    u64 one_dim_rules = 0;
 
+    void set_ruleset_dec(u64 dec) {
+	one_dim_rules = dec;
+	std::cout << "new ruleset = " << one_dim_rules << "\n";
+    }
+    void set_ruleset_bin(const char* rules_string) {
+	assert(type == ONE_DIM && "wrong type");
+	int i = 0;
+	int size = 0;
+	while(rules_string[i++] != '\0') {
+	    size++; 
+	}
+	std::cout << "size of ruleset = " << size << "\n";
+	i = 0;
+	while(size > 0) {
+	    size--;
+	    if (rules_string[i] == '1') {
+		BIT_SET(size, one_dim_rules);
+	    }
+	    i++;
+	}
+	std::cout << "new ruleset = " << one_dim_rules << "\n";
+    }
     void clear_cells() {
 	set_buf(cells, size, zero);
     }
 
     void randomize_cells() {
+	int limit = size;
+	if (type == ONE_DIM) limit = width;
 	for (int i = 0; i < size; ++i) {
-	    if (rand() / (RAND_MAX / 2)) {
+	    if (i < limit && rand() / (RAND_MAX / 2)) {
 		cells[i] = one;
 	    }
 	    else cells[i] = zero;
@@ -70,12 +100,13 @@ public:
     }
 
     void apply_rules() {
-	rules(*this);
-	if(type != ONE_DIM) {
+	if (type != ONE_DIM) {
+	    rules(*this);
 	    switch_buffers();
 	}
-	if(type == ONE_DIM && generation < height - 1) { 
-	    generation++;
+	else {
+	    one_dim_rules_func();
+	    if (generation < height - 1) generation++;
 	}
     }
     static void set_buf(T* buf, size_t size, T val) {
@@ -96,12 +127,10 @@ private:
     void setup_neighborhood() {
 	num_neighbors = neighbourhood_sizes[type];
 	neighbour_mask = new int[num_neighbors];
-	std::cout << "num neighbours = " << num_neighbors << "\n";
 	int index = 0;
 	switch(type) {
 	    case ONE_DIM:
 		for(int i = -1; i < 2; ++i) {
-		    std::cout << "index = " << index << "\n";
 		    assert(index < num_neighbors);
 		    neighbour_mask[index++] = i;
 		}
@@ -109,7 +138,6 @@ private:
 	    case TWO_DIM:
 		for (int y = -1; y <= 1; ++y) {
 		    for (int x = -1; x <= 1; ++x) {
-		    	std::cout << "index = " << index << "\n";
 			assert(index < num_neighbors);
 			int neighbor = INDEX(x, y, width);
 			neighbour_mask[index++] = neighbor;
@@ -118,6 +146,28 @@ private:
 	    break;
 	    default:
 		assert(0 && "unreachable");
+	}
+    }
+
+    void one_dim_rules_func() {
+	if (generation < height - 1) {
+	    int y = generation;
+	    for (int x = 0; x < width; ++x) {
+		T rule_index = 0;
+		T index = INDEX(x, y, width);
+		for (int n_i = 0; n_i < 3; ++n_i) {
+		    int new_index = INDEX(x, y, width) + neighbour_mask[n_i];
+		    if (new_index < 0) new_index += width;
+		    else if (new_index >= size) new_index -= width;
+		    if (cells[new_index] == one) {
+			rule_index |= (T)(1) << (2 - n_i);
+		    }
+		}
+		T new_value = BIT_AT(rule_index, one_dim_rules) ? one : zero;
+		assert(rule_index < 8);
+		assert(rule_index >= 0);
+		cells[INDEX(x, y + 1, width)] = new_value;
+	    }
 	}
     }
 };
--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
+#include "raygui.h"
 #include "raylib/src/raylib.h"
 #include "cell_automata.h"
-#include <cmath>
+#include <cinttypes>
 #include <iostream>
 #include <cassert>
 #include <string>
@@ -16,58 +17,39 @@ u32 dead_col  = 0xFF181818;
 KeyboardKey autoplay_key = KEY_SPACE;
 KeyboardKey next_frame_key = KEY_RIGHT;
 
-size_t cells_width = 200;
-size_t cells_height = 200;
+size_t cell_cols = 200;
+size_t cell_rows = 200;
+
 float window_width = 900;
 float window_height = 600;
 
 float min_dim = std::min(window_width, window_height);
 int controls_num_widgets = 10;
 Rectangle view_area = {0, 0, min_dim, min_dim};
+float cell_width = view_area.width / (float)cell_cols;
+float cell_height = view_area.height / (float)cell_rows;
 Rectangle control_area = {view_area.width, 0, window_width - view_area.width, window_height};
 Layout control_layout = Layout(control_area, VERTICAL, controls_num_widgets, 5);
 
 bool autoplay = false;
 double seconds_passed = 0.f;
-float initial_fps = 60.f;
-float target_fps = initial_fps;
 float max_fps = 100.f;
+float target_fps = 60;
 
 std::vector<Cell_Automat<u32>*> automata;
 Cell_Automat<u32>* selected_automat = NULL;
 int selected_automat_index = -1;
 
 Texture txt;
-Rectangle selected_pixel;
 
-void select_automat(int i) {
+Rectangle brush_view_rec = {0.f, 0.f, 1.f, 1.f};
+float brush_width = 1.f;
+float brush_height = 1.f;
+
+void automat_select(int i) {
     assert(i >= 0 && i < automata.size() && "wrong index");
     selected_automat_index = i;
     selected_automat = automata[i];
-}
-
-void one_dim_rules(Cell_Automat<u32>& automat) {
-    if (automat.generation < automat.height - 1) {
-	int ruleset[8] = {1, 0, 1, 0, 0, 1, 0, 1};
-	int y = automat.generation;
-	for (int x = 0; x < automat.width; ++x) {
-	    u32 rule_index = 0;
-	    u32 index = INDEX(x, y, automat.width);
-	    for (int n_i = 0; n_i < 3; ++n_i) {
-		int new_index = INDEX(x, y, automat.width) + automat.neighbour_mask[n_i];
-		if (new_index < 0) new_index += automat.width;
-		else if (new_index >= automat.size) new_index -= automat.width;
-		if (automat.cells[new_index] == automat.one) {
-		    rule_index |= (u32)(1) << (2 - n_i);
-		}
-	    }
-	    rule_index = 7 - rule_index;
-	    u32 new_value = ruleset[rule_index] ? automat.one : automat.zero;
-	    assert(rule_index < 8);
-	    assert(rule_index >= 0);
-	    automat.cells[INDEX(x, y + 1, automat.width)] = new_value;
-	}
-    }
 }
 
 void gol_rules(Cell_Automat<u32>& automat) {
@@ -103,6 +85,7 @@ void gol_rules(Cell_Automat<u32>& automat) {
 	}
     }
 }
+
 void resize() {
     window_width = GetScreenWidth();
     window_height = GetScreenHeight();
@@ -114,8 +97,13 @@ void resize() {
 		    .width = vertical ? window_width : window_width - view_area.width, 
 		    .height = vertical ? window_height - view_area.height : window_height};
     control_layout = Layout(control_area, VERTICAL, controls_num_widgets, 5);
+    cell_width = view_area.width / (float)cell_cols;
+    cell_height = view_area.height / (float)cell_rows;
+    brush_view_rec.width = cell_width * brush_width;
+    brush_view_rec.height = cell_height * brush_height;
+    control_layout.set_spacing(min_dim / 30.f);
 }
-
+ 
 void controls() {
     if (selected_automat && autoplay || IsKeyReleased(next_frame_key)) {
 	if (seconds_passed >= 1.f / target_fps) { 
@@ -125,95 +113,119 @@ void controls() {
 	}
     }
 
-    if (GuiButton(control_layout.get_slot(controls_num_widgets - 5), "erase")) {
+    if (selected_automat->type == ONE_DIM) {
+	bool secret_view = true;
+	Layout ruleset_layout = Layout(control_layout.get_slot(0), HORIZONTAL, 8, 5.f);
+	for(int i = 0; i < 8; ++i) {
+	    Layout vert_layout = Layout(ruleset_layout.get_slot(i), VERTICAL, 2);
+	    int bit = BIT_AT(7 - i, selected_automat->one_dim_rules);
+	    char bit_char = '0' + bit;
+	    GuiDrawText(&bit_char, vert_layout.get_slot(0), TEXT_ALIGN_MIDDLE, WHITE);
+	    if (GuiButton(vert_layout.get_slot(1), "flip")) {
+		if (bit) {
+		    BIT_RESET(7 - i, selected_automat->one_dim_rules);
+		}
+		else {
+		    BIT_SET(7 - i, selected_automat->one_dim_rules);
+		}
+	    }
+	}
+	std::string ruleset_string = std::to_string(selected_automat->one_dim_rules);
+	//GuiTextInputBox(control_layout.get_slot(1), "Input ruleset as decimal number", "message", 
+	//	 "buttons", "text", control_layout.get_slot(0).height, &secret_view);
+
+    }
+
+    if (GuiButton(control_layout.get_slot(controls_num_widgets - 5, true), "erase")) {
 	selected_automat->clear_cells();
 	UpdateTexture(txt, selected_automat->cells);
     }
-    GuiSlider(control_layout.get_slot(controls_num_widgets - 4), "0", std::to_string(max_fps).c_str(), &target_fps, 0.f, max_fps);
+    GuiSlider(control_layout.get_slot(controls_num_widgets - 4, true), "0", std::to_string(max_fps).c_str(), &target_fps, 0.f, max_fps);
 
-    if (GuiButton(control_layout.get_slot(controls_num_widgets - 3), "next automat")) {
+    if (GuiButton(control_layout.get_slot(controls_num_widgets - 3, true), "next automat")) {
 	selected_automat_index++;
 	selected_automat_index %= automata.size();
-	select_automat(selected_automat_index);
+	automat_select(selected_automat_index);
 	UpdateTexture(txt, selected_automat->cells);
     }
 
-    GuiToggle(control_layout.get_slot(controls_num_widgets - 2), "Play", &autoplay);
+    bool autoplay_prev = autoplay;
+    GuiToggle(control_layout.get_slot(controls_num_widgets - 2, true), "Play", &autoplay);
     if (IsKeyReleased(autoplay_key)) {
 	autoplay = !autoplay;
     }
+    if (autoplay_prev != autoplay) {
+	// one dimensional automat reached max height
+	if (selected_automat->type == ONE_DIM && selected_automat->generation == selected_automat->height - 1) {
+	    selected_automat->generation = 0;
+	}
+    }
 
-    if (GuiButton(control_layout.get_slot(controls_num_widgets - 1), "Restart")) {
+    if (GuiButton(control_layout.get_slot(controls_num_widgets - 1, true), "Restart")) {
 	selected_automat->generation = 0;
-	selected_automat->randomize_cells();
+	if (selected_automat->type != ONE_DIM) selected_automat->randomize_cells();
 	UpdateTexture(txt, selected_automat->cells);
     }
+
     Vector2 mouse_pos = GetMousePosition();
     if (CheckCollisionPointRec(mouse_pos, view_area)) {
-	float cell_width = view_area.width / (float)cells_width;
-	float cell_height = view_area.height / (float)cells_height;
-	selected_pixel = {mouse_pos.x - cell_width / 2.f , mouse_pos.y - cell_height / 2.f, cell_width, cell_height};
+	brush_view_rec.x = floor(mouse_pos.x - cell_width / 2.f); 
+	brush_view_rec.y = floor(mouse_pos.y - cell_height / 2.f);
 	if (IsMouseButtonDown(MOUSE_BUTTON_LEFT)) {
-	    Vector2 mouse_pos_projected = {mouse_pos.x / view_area.width * cells_width, mouse_pos.y / view_area.height * cells_height};
+	    
+	    Vector2 mouse_pos_projected = {mouse_pos.x / view_area.width * cell_cols, mouse_pos.y / view_area.height * cell_rows};
 	    selected_automat->cells[INDEX((int)mouse_pos_projected.x, (int)mouse_pos_projected.y, selected_automat->width)] = selected_automat->one;
 	    UpdateTexture(txt, selected_automat->cells);
 	}
+	DrawRectangleLinesEx(brush_view_rec, 2.f, WHITE);
     }
 }
 
-void add_automat(Cell_Automat<u32>* automat) {
-    automata.push_back(automat);
-    std::cout << "added automat\n";
+void draw_view_area() {
+    Rectangle source = {0, 0, (float)txt.width, (float)txt.height};
+    DrawTexturePro(txt, source, view_area, {0.f, 0.f}, 0.f, WHITE);
 }
-
 
 int main() {
     SetRandomSeed(GetTime());
     InitWindow(window_width, window_height, "hi");
     SetWindowState(FLAG_WINDOW_RESIZABLE);
-    SetTargetFPS(initial_fps);
-    Image h = GenImageColor(cells_width, cells_height, COLOR_FROM_U32(dead_col));
+    SetTargetFPS(max_fps);
+    Image h = GenImageColor(cell_cols, cell_rows, COLOR_FROM_U32(dead_col));
     txt = LoadTextureFromImage(h);
     UnloadImage(h);
 
-    Cell_Automat<u32> gol_automat(TWO_DIM, cells_width, cells_height, 
+    Cell_Automat<u32> gol_automat(TWO_DIM, cell_cols, cell_rows, 
 				   dead_col, alive_col, NULL, gol_rules);
-    Cell_Automat<u32> elem_automat(ONE_DIM, cells_width, cells_height, 
-				   dead_col, alive_col, NULL, one_dim_rules);
+    Cell_Automat<u32> elem_automat(ONE_DIM, cell_cols, cell_rows, 
+				   dead_col, alive_col);
     gol_automat.randomize_cells();
     elem_automat.randomize_cells();
+    elem_automat.set_ruleset_bin("1101");
+    elem_automat.set_ruleset_dec(30);
     automata.push_back(&gol_automat);
     automata.push_back(&elem_automat);
-    select_automat(0);
+    automat_select(1);
     UpdateTexture(txt, selected_automat->cells);
 
     std::cout << "alive color = " << alive_col << "\ndead color = " << dead_col << "\n";
-
+    control_layout.set_spacing(min_dim / 35.f);
 
     while (!WindowShouldClose()) {
-	if(target_fps > initial_fps) {
-	    SetTargetFPS(target_fps);
-	    initial_fps = target_fps;
-	}
 	double start = GetTime();
+
 	if (IsWindowResized()) {
 	    resize();
 	}
 
 	BeginDrawing();
 	ClearBackground(BLACK);
-	controls();
-	Rectangle source = selected_automat->type == ONE_DIM ? 
-	    Rectangle{0, 0, (float)txt.width, (float)selected_automat->generation} : 
-	    Rectangle{0, 0, (float)txt.width, (float)txt.height};
-	Rectangle dest = selected_automat->type == ONE_DIM ? 
-	    Rectangle{view_area.x, view_area.y, view_area.width, (float)selected_automat->generation * (view_area.height / (float)cells_height)} : 
-	    view_area;
-	DrawTexturePro(txt, source, dest, {0.f, 0.f}, 0.f, WHITE);
-	DrawRectangleLinesEx(selected_pixel, 5.f, WHITE);
 
-	DrawFPS(view_area.width, 0);
+	draw_view_area();
+	controls();
+
 	EndDrawing();
+
 	double end = GetTime();
 	seconds_passed += end - start;
     }
