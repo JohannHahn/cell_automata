fe1222a002bd8ca525d21a2bff940d96c0b4f638
Johann Hahn <wanische@gmail.com>
2024-02-12 19:42:54 +0100

    creating new automat now possible

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include "raylib/src/raylib.h"
 #include "cell_automata.h"
 #include <cinttypes>
+#include <cmath>
 #include <cstring>
 #include <iostream>
 #include <cassert>
@@ -12,11 +13,12 @@ typedef uint32_t u32;
 
 #define COLOR_FROM_U32(c) *(Color*)&c
 
-enum control_view_type {
-    CURRENT, NEXT, CONTROL_VIEW_MAX
+enum state {
+    VIEW_CURRENT, PREPARE_NEXT, STATE_MAX
 };
-control_view_type control_window_type = CURRENT;
+state state = VIEW_CURRENT;
 bool mouse_draw = true;
+bool debugging = false;
 
 u32 alive_col = 0xFFFF1111;
 u32 dead_col  = 0xFF181818;
@@ -24,11 +26,15 @@ u32 dead_col  = 0xFF181818;
 KeyboardKey autoplay_key = KEY_SPACE;
 KeyboardKey next_frame_key = KEY_RIGHT;
 
-size_t cell_cols = 200;
-size_t cell_rows = 200;
+int cell_cols = 200;
+int cell_rows = 200;
+int max_cols = 1000;
+int max_rows = 1000;
+int min_cols = 10;
+int min_rows = 10;
 
-float window_width = 900;
-float window_height = 600;
+float window_width = 1200;
+float window_height = 900;
 
 float min_dim = std::min(window_width, window_height);
 Rectangle view_area = {0, 0, min_dim, min_dim};
@@ -38,13 +44,20 @@ Rectangle control_area = {view_area.width, 0, window_width - view_area.width, wi
 int controls_num_widgets = 10;
 Layout control_layout = Layout(control_area, VERTICAL, controls_num_widgets, 5);
 int control_index = 0;
+bool automat_type_selection = 0;
 
 bool autoplay = false;
 double seconds_passed = 0.f;
 float max_fps = 100.f;
 float target_fps = 60;
 
-Cell_Automat<u32> active_automat;
+Cell_Automat<u32>* active_automat;
+Cell_Automat<u32>* next_automat;
+float next_cell_cols = 0;
+float next_cell_rows = 0;
+u64 next_one_dim_ruleset = 0;
+u32* next_input = NULL;
+Cell_Automat<u32>* prev_automat;
 
 Texture txt;
 
@@ -52,39 +65,6 @@ Rectangle brush_view_rec = {0.f, 0.f, 1.f, 1.f};
 float brush_width = 1.f;
 float brush_height = 1.f;
 
-void gol_rules(Cell_Automat<u32>& automat) {
-    for (int y = 0; y < automat.height; ++y) {
-	for (int x = 0; x < automat.width; ++x) {
-	    int index = x + y * automat.width;
-	    u32 input_val = automat.cells[index];
-	    if (input_val != automat.one && input_val != automat.zero) {
-		std::cout << "wrong colors! input_val = " << input_val << "\n";
-		std::cout << "alive color = " << alive_col << "\ndead color = " << dead_col << "\n";
-	    }
-
-	    int neighbours = 0; 
-	    for (int i = 0; i < automat.num_neighbors; ++i) {
-		int new_index = index + automat.neighbour_mask[i];
-		if(new_index < 0) new_index += automat.width;
-		else if(new_index >= automat.size) new_index -= automat.width;
-		if (new_index != index && automat.cells[new_index] != automat.zero) {
-		    neighbours++;
-		}
-	    }
-
-	    bool alive = (input_val == automat.one);
-	    if(alive && neighbours == 3) automat.empty[index] = automat.one;
-	    else if(alive && neighbours == 2) automat.empty[index] = automat.one;
-	    else if(alive && neighbours < 2) automat.empty[index] = automat.zero;
-	    else if(alive && neighbours > 3) automat.empty[index] = automat.zero;
-	    else if(!alive && neighbours == 3) automat.empty[index] = automat.one;
-	    else if(!alive && neighbours != 3) automat.empty[index] = automat.zero;
-	    else {
-		assert(0 && "unreachable");
-	    }
-	}
-    }
-}
 
 void resize() {
     window_width = GetScreenWidth();
@@ -104,34 +84,55 @@ void resize() {
     control_layout.set_spacing(min_dim / 30.f);
 }
 
+void set_active(Cell_Automat<u32>* active) {
+    prev_automat = active_automat;
+    active_automat = active;
+}
+
+void switch_to_next() {
+    assert(state != PREPARE_NEXT && "switching from next to next");
+    state = PREPARE_NEXT;
+    set_active(next_automat);
+    autoplay = false;
+}
+
+void switch_back_to_current() {
+    assert(prev_automat->is_initialized() && "no current to switch back too");
+    state = VIEW_CURRENT;
+    set_active(prev_automat);
+}
+
+Rectangle get_next_control_slot(bool spaced = true) {
+    return control_layout.get_slot(control_index++, spaced);
+}
+
 void control_current_automat() {
     if (autoplay || IsKeyReleased(next_frame_key)) {
 	if (seconds_passed >= 1.f / target_fps) { 
 	    seconds_passed = 0.f;
-	    active_automat.apply_rules(); 
-	    UpdateTexture(txt, active_automat.cells);
+	    active_automat->apply_rules(); 
 	}
     }
     // info about current layout
-    Layout info_layout = Layout(control_layout.get_slot(control_index++, true), HORIZONTAL, 2, 1.f);
-    float text_height = info_layout.get_slot(0).height;
-    std::string type = active_automat.type == ONE_DIM ? "Type:\n Elementary\n One dimensional" : "Type:\n Two dimensional"; type += "\n";
-    GuiDrawText(type.c_str(), info_layout.get_slot(0, true), TEXT_ALIGN_LEFT, WHITE);
-    std::string dims = "Width = " + std::to_string(active_automat.width) + "\nHeight = " + std::to_string(active_automat.height) + "\n";
-    GuiDrawText(dims.c_str(), info_layout.get_slot(1, true), TEXT_ALIGN_LEFT, WHITE);
+    std::string table_body = active_automat->type == ONE_DIM ? "1D elementary" : "2D Game of life"; table_body += '\0';
+    table_body += std::to_string(active_automat->width); table_body += '\0';
+    table_body += std::to_string(active_automat->height); table_body += '\0';
+    Gui::table(get_next_control_slot(), 3, 1, "Type\0Width\0Height", table_body.c_str());; 
     
-    info_layout = Layout(control_layout.get_slot(control_index++, true), HORIZONTAL, 2, 1.f);
-    GuiDrawText("Ruleset:", info_layout.get_slot(0, true), TEXT_ALIGN_LEFT, WHITE);
-    std::string ruleset_str = active_automat.type == TWO_DIM ? "Conway's game of life" : "";
-    GuiDrawText(ruleset_str.c_str(), info_layout.get_slot(1, true), TEXT_ALIGN_LEFT, WHITE);
+
+    Layout ruleset_info_layout = Layout(get_next_control_slot(), SLICE_VERT, 0.1f, 1.f);
+    Layout ruleset_label_layout = Layout(ruleset_info_layout.get_slot(0), HORIZONTAL, 2, 1.f);
+    GuiDrawText("Ruleset:", ruleset_label_layout.get_slot(0, true), TEXT_ALIGN_LEFT, WHITE);
+    std::string ruleset_str = active_automat->type == TWO_DIM ? "Conway's game of life" : std::to_string(active_automat->one_dim_rules);
+    GuiDrawText(ruleset_str.c_str(), ruleset_label_layout.get_slot(1, true), TEXT_ALIGN_LEFT, WHITE);
     // input one dimensional rules as binary
-    if (active_automat.type == ONE_DIM) {
+    if (active_automat->type == ONE_DIM) {
 	bool secret_view = true;
-	Layout ruleset_layout = Layout(control_layout.get_slot(control_index++), HORIZONTAL, 8, 5.f);
+	Layout ruleset_layout = Layout(ruleset_info_layout.get_slot(1), HORIZONTAL, 8, 5.f);
 	for(int i = 0; i < 8; ++i) {
 	    std::string binary = "000";
 	    Layout vert_layout = Layout(ruleset_layout.get_slot(i), VERTICAL, 2);
-	    int bit = BIT_AT(7 - i, active_automat.one_dim_rules);
+	    int bit = BIT_AT(7 - i, active_automat->one_dim_rules);
 	    std::string bit_str; 
 	    bit_str += '0' + bit;
 	    for (int j = 0; j < 3; ++j) {
@@ -141,23 +142,88 @@ void control_current_automat() {
 	    GuiDrawText(bit_str.c_str(), vert_layout.get_slot(0), TEXT_ALIGN_MIDDLE, WHITE);
 	    if (GuiButton(vert_layout.get_slot(1), binary.c_str())) {
 		if (bit) {
-		    BIT_RESET(7 - i, active_automat.one_dim_rules);
+		    BIT_RESET(7 - i, active_automat->one_dim_rules);
 		}
 		else {
-		    BIT_SET(7 - i, active_automat.one_dim_rules);
+		    BIT_SET(7 - i, active_automat->one_dim_rules);
 		}
 	    }
+	}
+    }
+    GuiSlider(get_next_control_slot(), "0", std::to_string(max_fps).c_str(), &target_fps, 0.f, max_fps);
+
+    bool autoplay_prev = autoplay;
+    GuiToggle(get_next_control_slot(), "Play", &autoplay);
+    if (IsKeyReleased(autoplay_key)) {
+	autoplay = !autoplay;
+    }
+    if (autoplay_prev != autoplay) {
+	// one dimensional automat reached max height
+	if (active_automat->type == ONE_DIM && active_automat->generation == active_automat->height - 1) {
+	    active_automat->generation = 0;
+	}
+    }
+
+    if (GuiButton(get_next_control_slot(), "Restart")) {
+	active_automat->generation = 0;
+	memcpy(active_automat->cells, active_automat->initial_cells, active_automat->size);
+	//autoplay = false;
+    }
+}
+
+void control_next_automat() {
+    GuiToggle(get_next_control_slot(), automat_type_selection ? "Type: 2D" : "Type: 1D", &automat_type_selection);
+
+    GuiSlider(get_next_control_slot(), std::to_string(min_cols).c_str(), std::to_string(max_cols).c_str(), &next_cell_cols, min_cols, max_cols);
+    GuiSlider(get_next_control_slot(), std::to_string(min_rows).c_str(), std::to_string(max_rows).c_str(), &next_cell_rows, min_rows, max_rows);
+    next_cell_cols = round(next_cell_cols);
+    next_cell_rows = round(next_cell_rows);
+
+    if (GuiButton(get_next_control_slot(), "Apply\n(empties buffer)")) {
+	active_automat->init((Automata_Type)automat_type_selection, next_cell_cols, next_cell_rows, dead_col, alive_col);
+	std::cout << "Apply: after reiniting the automat\n";
+
+	UnloadTexture(txt);
+	Image h = GenImageColor(next_cell_cols, next_cell_rows, *(Color*)&dead_col);
+	txt = LoadTextureFromImage(h);
+	UnloadImage(h);
+
+	if (automat_type_selection == TWO_DIM) {
+	    active_automat->set_rules_gol();
+	    std::cout << "Apply: after setting rules to game of life\n";
+	}
+	else {
+	    active_automat->set_ruleset_dec(next_one_dim_ruleset);
+	    std::cout << "Apply: after setting rules to next_one_dim_ruleset\n";
+	}
+	if (next_input) {
+	    active_automat->set_cells(next_input);
+	    std::cout << "Apply: after setting input\n";
+	}
+	std::cout << "Apply: after apply\n";
+	assert(active_automat->rules && "rules not set on the new automat");
+    }
+
+    if (active_automat->is_initialized()) {
+	if (GuiButton(get_next_control_slot(), "Start with current buffer")) {
+	    state = VIEW_CURRENT;
 	}
     }
 }
 
 void control_window_selection() {
-    Layout top_row_layout = Layout(control_layout.get_slot(control_index++, true), HORIZONTAL, CONTROL_VIEW_MAX, 5.f);
+    Layout top_row_layout = Layout(get_next_control_slot(), HORIZONTAL, STATE_MAX, 5.f);
     if (GuiButton(top_row_layout.get_slot(0, true), "Edit current automat")) {
-	control_window_type = CURRENT;
+	if (state != VIEW_CURRENT) {
+	    switch_back_to_current();
+	    active_automat->print();
+	}
     }
     if (GuiButton(top_row_layout.get_slot(1, true), "Prepare next automat")) {
-	control_window_type = NEXT;
+	if (state != PREPARE_NEXT) {
+	    switch_to_next();
+	    active_automat->print();
+	}
     }
     //top_row_layout.draw();
 }
@@ -166,45 +232,33 @@ void controls() {
     control_index = 0;
     control_window_selection();
 
-    if (control_window_type == CURRENT) {
+    if (state == VIEW_CURRENT) {
 	control_current_automat();
     }
-    if (GuiButton(control_layout.get_slot(control_index++, true), "erase")) {
-	active_automat.clear_cells();
-	UpdateTexture(txt, active_automat.cells);
-    }
-    GuiSlider(control_layout.get_slot(control_index++, true), "0", std::to_string(max_fps).c_str(), &target_fps, 0.f, max_fps);
-
-    bool autoplay_prev = autoplay;
-    GuiToggle(control_layout.get_slot(control_index++, true), "Play", &autoplay);
-    if (IsKeyReleased(autoplay_key)) {
-	autoplay = !autoplay;
-    }
-    if (autoplay_prev != autoplay) {
-	// one dimensional automat reached max height
-	if (active_automat.type == ONE_DIM && active_automat.generation == active_automat.height - 1) {
-	    active_automat.generation = 0;
-	}
+    else {
+	control_next_automat();
     }
 
-    if (GuiButton(control_layout.get_slot(control_index++, true), "Restart")) {
-	active_automat.generation = 0;
-	memcpy(active_automat.cells, active_automat.initial_cells, active_automat.size);
-	//autoplay = false;
-	UpdateTexture(txt, active_automat.cells);
+
+    if (GuiButton(get_next_control_slot(), "randomize buffer")) {
+	active_automat->randomize_cells();
+    }
+    if (GuiButton(get_next_control_slot(), "erase buffer")) {
+	active_automat->clear_cells();
     }
 
-    GuiCheckBox(control_layout.get_slot(control_index++, true), "Mouse drawing", &mouse_draw);
-    if (mouse_draw) {
+    Rectangle mouse_checkbox_rec = get_next_control_slot();
+    mouse_checkbox_rec.width /= 5.f;
+    GuiCheckBox(mouse_checkbox_rec, "Mouse drawing", &mouse_draw);
+    if (mouse_draw && active_automat->is_initialized()) {
 	Vector2 mouse_pos = GetMousePosition();
 	if (CheckCollisionPointRec(mouse_pos, view_area)) {
 	    brush_view_rec.x = floor(mouse_pos.x - cell_width / 2.f); 
 	    brush_view_rec.y = floor(mouse_pos.y - cell_height / 2.f);
 	    if (IsMouseButtonDown(MOUSE_BUTTON_LEFT)) {
 		
-		Vector2 mouse_pos_projected = {mouse_pos.x / view_area.width * cell_cols, mouse_pos.y / view_area.height * cell_rows};
-		active_automat.cells[INDEX((int)mouse_pos_projected.x, (int)mouse_pos_projected.y, active_automat.width)] = active_automat.one;
-		UpdateTexture(txt, active_automat.cells);
+		Vector2 mouse_pos_projected = {mouse_pos.x / view_area.width * active_automat->width, mouse_pos.y / view_area.height * active_automat->height};
+		active_automat->cells[INDEX((int)mouse_pos_projected.x, (int)mouse_pos_projected.y, active_automat->width)] = active_automat->one;
 	    }
 	    DrawRectangleLinesEx(brush_view_rec, 2.f, WHITE);
 	}
@@ -225,11 +279,12 @@ int main() {
     txt = LoadTextureFromImage(h);
     UnloadImage(h);
 
-    active_automat.init(ONE_DIM, cell_cols, cell_rows, dead_col, alive_col);
-    active_automat.randomize_cells();
-    active_automat.set_ruleset_bin("1101");
+    active_automat = new Cell_Automat<u32>(ONE_DIM, cell_cols, cell_rows, dead_col, alive_col);
+    active_automat->randomize_cells();
+    active_automat->set_ruleset_dec(30);
+    next_automat = new Cell_Automat<u32>(TWO_DIM, cell_cols, cell_rows, dead_col, alive_col);
 
-    UpdateTexture(txt, active_automat.cells);
+    UpdateTexture(txt, active_automat->cells);
 
     std::cout << "alive color = " << alive_col << "\ndead color = " << dead_col << "\n";
     control_layout.set_spacing(min_dim / 50.f);
@@ -247,6 +302,7 @@ int main() {
 	draw_view_area();
 	controls();
 
+	UpdateTexture(txt, active_automat->cells);
 	EndDrawing();
 
 	double end = GetTime();
